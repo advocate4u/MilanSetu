66c43eedc38a8103f23b5c88f1c9ccb3fb497f96
advocate4u <advocatemittal4u@gmail.com>
2026-09-10 18:32:42 +0530

    Mount authenticated discovery experience

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import DiscoveryPanel from './DiscoveryPanel'
 import NotificationPanel from './NotificationPanel'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <DiscoveryPanel />
     <NotificationPanel />
   </StrictMode>,
 )
