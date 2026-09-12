cc12ca167f9cd4a0d01a71385dc5aeb500ecadb9
advocate4u <advocatemittal4u@gmail.com>
2026-09-12 12:12:32 +0530

    Add profile photo manager to app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,17 @@ import App from './App'
 import DiscoveryPanel from './DiscoveryPanel'
 import NotificationPanel from './NotificationPanel'
 import ProfileEditor from './ProfileEditor'
+import ProfilePhotoManager from './ProfilePhotoManager'
+import MessagingPanel from './MessagingPanel'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <ProfileEditor />
+    <ProfilePhotoManager />
     <DiscoveryPanel />
+    <MessagingPanel />
     <NotificationPanel />
   </StrictMode>,
 )
