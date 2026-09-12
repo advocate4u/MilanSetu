bca85de681bec0699489bc99dde0d95313125496
advocate4u <advocatemittal4u@gmail.com>
2026-09-12 12:30:40 +0530

    Integrate profile photo manager

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import App from './App'
 import DiscoveryPanel from './DiscoveryPanel'
 import NotificationPanel from './NotificationPanel'
 import ProfileEditor from './ProfileEditor'
+import ProfilePhotoManager from './ProfilePhotoManager'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <ProfileEditor />
+    <ProfilePhotoManager />
     <DiscoveryPanel />
     <NotificationPanel />
   </StrictMode>,
