62a255b7f76ebe56a6803209214efaf3299dc9f2
advocate4u <advocatemittal4u@gmail.com>
2026-09-12 12:29:10 +0530

    Complete profile editing experience (#13)
    * Add complete profile extended-data API
    
    * Add profile editor API client
    
    * Add complete profile editor UI
    
    * Mount profile editor
    
    * Style profile editor
    
    * Fix profile editor backend build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import DiscoveryPanel from './DiscoveryPanel'
 import NotificationPanel from './NotificationPanel'
+import ProfileEditor from './ProfileEditor'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ProfileEditor />
     <DiscoveryPanel />
     <NotificationPanel />
   </StrictMode>,
