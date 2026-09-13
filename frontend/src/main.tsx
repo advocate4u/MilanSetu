7b98ed1bf6ec161411787a694fe30eacbb09951f
advocate4u <advocatemittal4u@gmail.com>
2026-09-13 18:41:13 +0530

    Register reviewer workspace UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,11 +7,13 @@ import NotificationPanel from './NotificationPanel'
 import ProfileEditor from './ProfileEditor'
 import ProfilePhotoManager from './ProfilePhotoManager'
 import VerificationPanel from './VerificationPanel'
+import ReviewerPanel from './ReviewerPanel'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ReviewerPanel />
     <ProfileEditor />
     <ProfilePhotoManager />
     <VerificationPanel />
