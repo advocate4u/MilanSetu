e7aa4d4af8df33fcee808c5b8de73d04d931fe33
advocate4u <advocatemittal4u@gmail.com>
2026-09-16 20:34:46 +0530

    Route frontend through authenticated app shell

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import AuthenticatedWorkspace from './AuthenticatedWorkspace'
+import AppShell from './AppShell'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <AuthenticatedWorkspace />
+    <AppShell />
   </StrictMode>,
 )
